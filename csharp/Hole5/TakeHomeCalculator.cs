db8a5f13bb42e140e7b707422dcc110a23d419e0
Rufino Perez <ruperez@aidacanarias.com>
2022-06-06 09:05:58 +0200

    Hole 5 - 16 points

--- csharp/Hole5/TakeHomeCalculator.cs
+++ csharp/Hole5/TakeHomeCalculator.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Hole5
 {
     public class TakeHomeCalculator
     {
-        private readonly int percent;
+        private readonly TaxRate taxRate;
 
-        public TakeHomeCalculator(int percent)
+        public TakeHomeCalculator(TaxRate taxRate)
         {
-            this.percent = percent;
+            this.taxRate = taxRate;
         }
 
         public Money NetAmount(Money first, params Money[] rest)
@@ -24,9 +23,7 @@ namespace Hole5
                 total = total.Plus(next);
             }
 
-            Double amount = total.value * (percent / 100d);
-            Money tax = Money.Create(Convert.ToInt32(amount), first.currency);
-
+            Money tax = taxRate.Apply(total);
             return total.Minus(tax);
         }
     }
